a1ad09b639938bd0e788a82f34aaa1eb0dce63c3
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-02-14 23:23:11 +0300

    print command

---
+++ week1/main.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Bir değeri console da görmek, yazdırmak için kullanılıyor.
+  // Debug ederken kontrol için de kullanabilabiliyor.
+  // İçerisine yazılan her şey nesnedir. (object)
+  print('Merhaba Dünya');
+
+  // Karakter Tanımlaması
+  print('Merhaba' + ' ' + 'Dünya');
+
+  // Numara Tanımlaması
+  print(2 + 3);
+}
