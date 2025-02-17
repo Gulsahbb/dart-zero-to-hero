5d6bd24c40d731f59448ff1c5c5b4d8625ed1f74
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-02-17 16:41:23 +0300

    if else example

--- week2/conditial.dart
+++ week2/conditial.dart
@@ -1,5 +1,5 @@
 void main() {
-  int money = 15;
+  /* int money = 15;
   String userName = 'Gulsah';
   print('$money' + userName);
 
@@ -29,5 +29,39 @@ void main() {
     print('Lütfen sıra alınız.');
   } else {
     print('Lütfen kredinizi ödeyin.');
+  }*/
+
+  // Bir mağazaya isim verilecek.
+  // Örnek isimler toplanır.
+  // Örnke isimler : Ahmet, Veli, Mehmet, kx
+  // mağaza : Ben sadece karakter uzunluğu sadece iki veya daha altı olanları görmek istiyorum.
+
+  final String ahmetCompany = 'Ahmet';
+  final String mehmetCompany = 'Mehmet';
+  final String veliCompany = 'Veli';
+  final String kxCompany = 'Kx';
+  final String xCompany = 'x';
+  const int companyLength = 4;
+  String results = '';
+
+  if (ahmetCompany.length <= companyLength) {
+    results += ahmetCompany;
+  }
+  if (mehmetCompany.length <= companyLength) {
+    results += mehmetCompany;
+  }
+  if (veliCompany.length <= companyLength) {
+    results += veliCompany;
+  }
+  if (kxCompany.length <= companyLength) {
+    results += kxCompany;
+  }
+  if (xCompany.length <= companyLength) {
+    results += xCompany;
+  }
+  if (results.length <= 0) {
+    print('Mağaza ismi bulunamadı.');
+  } else {
+    print('İstenilen Şartlara Göre Mağaza isimleri : $results');
   }
 }
