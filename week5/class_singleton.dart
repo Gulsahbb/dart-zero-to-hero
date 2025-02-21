180bc206cead2d3350133e1e05c35dab61ff39b6
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-02-21 21:18:37 +0300

    Factory Constructors

--- week5/class_singleton.dart
+++ week5/class_singleton.dart
@@ -1,8 +1,14 @@
 void main(List<String> args) {
-  final newProduct = Product.money;
+  //  final newProduct = Product.money;
   calculateMoney();
   // Bir tane örenk olsun her yerden kullanılsın.
   // Kullanımı riskli her yerde kullanılması.
+
+  // user classını kullanarak product yapmak istiyorum.
+  final user1 = User('veli', 'a');
+  final newProduct = Product(user1.product);
+  final newProduct2 = Product.fromuser(user1);
+  Product.veli();
 }
 
 void calculateMoney() {
@@ -15,7 +21,13 @@ void calculateMoney() {
 
 class Product {
   static int? money = 10;
+  String name;
+  Product(this.name);
+  Product.veli([this.name = 'veli']);
 
+  factory Product.fromuser(User user) {
+    return Product(user.name);
+  }
   static void incrementMoney(int newMoney) {
     if (money != null) {
       money = money! + newMoney;
@@ -23,4 +35,9 @@ class Product {
   }
 }
 
-class User {}
+class User {
+  final String name;
+  final String product;
+
+  User(this.name, this.product);
+}
